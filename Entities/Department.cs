d83baa646038cf8827019067bac58bca07fd6548
Rafael Dos Santos <raphael_979@hotmail.com>
2019-09-18 17:10:22 -0300

    Primeiro foi criado uma classe Departament com Name

---
+++ Entities/Department.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course.Entities
+{
+    class Department
+    {
+        public string Nome { get; set; }
+        public Department()
+        {
+
+        }
+
+        public Department(string nome)
+        {
+            Nome = nome;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -8,17 +8,8 @@ namespace Course
     {
         static void Main(string[] args)
         {
-            Order order = new Order
-            {
-                Id = 1080,
-                Moment = DateTime.Now,
-                Status = OrderStatus.PendingPayment
-            };
-            Console.WriteLine(order);
-            String txt = OrderStatus.PendingPayment.ToString();
-            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
-            Console.WriteLine(txt);
-            Console.WriteLine(os);
+
+            
         }
     }
 }
