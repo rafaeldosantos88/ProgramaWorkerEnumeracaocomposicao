bc9d3d6107e7c54840e3ec233457598dc6fd7e9b
Rafael Dos Santos <raphael_979@hotmail.com>
2019-09-18 17:32:39 -0300

    2-Etapa foi criado Classe HourContract aonde tem Date,ValuePerHour(Valor por hora),Hours.Ao colocar as Variaveis,foi colocado construtor padrao e os demais construtores.Depois foi colocado um metodo que multiplica as horas Hours x valuePerHour ou seja valor por horas

---
+++ Entities/HourContract.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course.Entities
+{
+    class HourContract
+    {
+        public DateTime Date { get; set; }
+        public double ValuePerHour { get; set; }
+        public int hours { get; set; }
+
+public HourContract()
+        {
+
+        }
+
+        public HourContract(DateTime date, double valuePerHour, int hours)
+        {
+            Date = date;
+            ValuePerHour = valuePerHour;
+            this.hours = hours;
+        }
+        public double TotalValue()
+        {
+            return hours * ValuePerHour
+        }
+    }
+}
